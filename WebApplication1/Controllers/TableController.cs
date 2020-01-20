71f31e6c30267aa3b25a9a56c343b2d137b1306c
nakap <nakapyndzyn@gmail.com>
2020-01-20 23:21:12 +0200

    added table rendering and calculation

---
+++ WebApplication1/Controllers/TableController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Context;
+using WebApplication1.Helpers;
+
+namespace WebApplication1.Views.Operations
+{
+    public class TableController : Controller
+    {
+        public ContextAdapter adapter = new ContextAdapter();
+        public OperationHelper operationHelper = new OperationHelper();
+        public TableBuilder tableBuilder = new TableBuilder();
+
+        public ActionResult Index()
+        {
+            var actions = adapter.GetOperations();
+
+            ViewBag.Labels = tableBuilder.GetCodeLabels(actions);
+            ViewBag.Durations = tableBuilder.GetDurations(actions);
+            List<List<int>> list = tableBuilder.GetIndicatiorsOfWork(actions);
+            ViewBag.DurationsRP = list[0];
+            ViewBag.DurationsRZ = list[1];
+            ViewBag.DurationsPZ = list[2];
+            ViewBag.DurationsPP = list[3];
+            ViewBag.DurationsP = list[4];
+            ViewBag.DurationsB = list[5];
+
+            return View();
+        }
+    }
+}
